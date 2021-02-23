94c1f0cd9d7b04bcd240e25661427fe8fb4309d3
jykim-rust <alexj99@naver.com>
2021-02-23 16:38:07 +0900

    first project

--- odd.py
+++ odd.py
@@ -1,11 +1,15 @@
-from datetime import datetime
+from datetime import datetime 
+import time
+import random
+
 odds = [1,3,5,7,9,11,13,15,17,19,
         21,23,25,27,29,31,33,35,37,39,
         41,43,45,47,49,51,53,55,57,59]
 
 right_this_minute= datetime.today().minute
-
-if right_this_minute in odds:
-    print("odd time")
-else:
-    print("not odd")
+for i in range(5):
+    if right_this_minute in odds:
+        print("odd time" ,i)
+    else:
+        print("not odd",i)
+    time.sleep(random.randint(1,3))
